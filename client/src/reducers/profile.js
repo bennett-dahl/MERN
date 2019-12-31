dec4647a67e75466b531e9508d02606d95b81fd3
Bennett Dahl <bennett.dahl@gmail.com>
2019-12-31 11:30:25 -0800

    Profile Reducer & Get Current Profile

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,18 @@
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {}
+};
+
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case value:
+
+    default:
+      return state;
+  }
+}
