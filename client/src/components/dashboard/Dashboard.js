02a24ec94be62734b3b4860850278099c5e5387b
Bennett Dahl <bennett.dahl@gmail.com>
2020-01-10 17:02:23 -0800

    Error in Delete functionality, improper function execution due to passing through Dashboard.js?

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Spinner } from '../layout/Spinner';
+import { DashboardActions } from './DashboardActions';
+import { getCurrentProfile, deleteEducation, deleteExperience } from '../../actions/profile';
+import { Education } from './Education';
+import { Experience } from './Experience';
 
-const Dashboard = props => {
-  return <div>Dashboard</div>;
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>
+          <DashboardActions />
+          <Experience experience={profile.experience} deleteExperience={deleteExperience()} />
+          <Education education={profile.education} deleteEducation={deleteEducation()} />
+        </Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
 
-export default Dashboard;
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, { getCurrentProfile, deleteEducation, deleteExperience })(Dashboard);
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,3 +1,10 @@
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE
+} from '../actions/types';
+
 const initialState = {
   profile: null,
   profiles: [],
@@ -10,8 +17,26 @@ export default function(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
-    case value:
-
+    case GET_PROFILE:
+    case UPDATE_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false
+      };
     default:
       return state;
   }
